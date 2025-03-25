b0b67a9d4a94f2b99f799db44009e2553e545d8d
Mintu Ratan Das <minturatan.das@finconsgroup.com>
2025-03-25 18:54:46 +0100

    sort anonymous Comparator compare

--- src/main/java/org/advance/java/Main.java
+++ src/main/java/org/advance/java/Main.java
@@ -2,6 +2,7 @@ package org.advance.java;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,7 +15,12 @@ public class Main {
         books.add(book2);
         books.add(book3);
 
-        Collections.sort(books, new MyComparator());
+        Collections.sort(books, new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return  o1.getTitle().compareTo(o2.getTitle());
+            }
+        });
 
         for (Book book : books) {
             System.out.println(book.getTitle());
